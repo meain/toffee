7f29fc7a30d0477090f9207c99f3b281233228fc
Abin Simon <abinsimon10@gmail.com>
2020-11-28 11:47:43 +0530

    basic stuff working

---
+++ fixtures/python/pytest/test_stuff.py
@@ -0,0 +1,16 @@
+class TestClass:
+    class TestNestedClass:
+        def test_nestedclass_method(self):
+            assert 2 == 2
+
+    def test_method(self):
+        assert 1 == 1
+
+
+class TestClassObj(object):
+    def test_method_obj(self):
+        assert 1 == 1
+
+
+def test_function():
+    assert 1 == 1
